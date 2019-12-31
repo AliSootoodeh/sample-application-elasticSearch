379b50989035c0078889fa503916c9fd493d5472
ali sootoodeh <alisotudehsjn@yahoo.com>
2020-01-01 03:02:03 +0330

    indexing and mapping functions attached

--- server/connection.js
+++ server/connection.js
@@ -1,1 +1,52 @@
-const elasticsearch = require('elasticsearch')
+const elasticsearch = require('elasticsearch');
+
+
+// Core ES variables for this project
+const index = 'library';
+const type = 'novel';
+const port = 9200;
+const host = process.env.ES_HOST || 'localhost';
+const client = new elasticsearch.Client({ host: { host, port } });
+
+/** Check the ES connection status */
+async function checkConnection () {
+    let isConnected = false
+    while (!isConnected) {
+        console.log('Connecting to ES')
+        try {
+        const health = await client.cluster.health({})
+        console.log(health)
+        isConnected = true
+        } catch (err) {
+        console.log('Connection Failed, Retrying...', err)
+        }
+    }
+}
+
+/** Add book section schema mapping to ES */
+async function putBookMapping () {
+    const schema = {
+        title: { type: 'keyword' },
+        author: { type: 'keyword' },
+        location: { type: 'integer' },
+        text: { type: 'text' }
+    }
+
+    return client.indices.putMapping({include_type_name:true ,index, type, body: { properties: schema } })
+}
+
+/** Clear the index, recreate it, and add mappings */
+async function resetIndex () {
+    if (await client.indices.exists({ index })) {
+        await client.indices.delete({ index })
+    }
+
+    await client.indices.create({ index })
+    await putBookMapping()
+}
+
+// checkConnection()
+
+module.exports = {
+    client, index, type, checkConnection, resetIndex
+}
