a5fc9410df9bbf2c83bd628a68aa6022603e7db1
ali sootoodeh <alisotudehsjn@yahoo.com>
2020-01-01 09:55:30 +0330

    elasticsearch approved

--- server/app.js
+++ server/app.js
@@ -1,13 +1,46 @@
-const Koa = require("koa");
-const app = new Koa();
+const Koa = require('koa')
+const Router = require('koa-router')
+const joi = require('joi')
+const validate = require('koa-joi-validate')
+const search = require('./search')
 
+const app = new Koa()
+const router = new Router()
+
+// Log each request to the console
 app.use(async (ctx, next) => {
-    ctx.body = "Hello World From the Backend Container";
-});
+    const start = Date.now()
+    await next()
+    const ms = Date.now() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}`)
+})
 
-const port = process.env.PORT || 3000;
+// Log percolated errors to the console
+app.on('error', err => {
+    console.error('Server Error', err)
+})
 
-app.listen(port, err => {
-    if (err) console.error(err);
-    console.log(`App Listening on Port ${port}`);
-});
+// Set permissive CORS header
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*')
+    return next()
+})
+
+/**
+ * GET /search
+ * Search for a term in the library
+ */
+router.get('/search', async (ctx, next) => {
+    const { term, offset } = ctx.request.query
+    ctx.body = await search.queryTerm(term, offset)
+})
+
+const port = process.env.PORT || 3000
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(port, err => {
+    if (err) throw err
+    console.log(`App Listening on Port ${port}`)
+})
